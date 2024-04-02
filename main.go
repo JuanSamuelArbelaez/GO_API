f0c7dbe33112abdd7fd0e3c2e5499785c8d3bfac
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-04-02 18:50:37 -0500

    Various people and misc routes created.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/JuanSamuelArbelaez/GO_API/SQL"
-	"github.com/JuanSamuelArbelaez/GO_API/Utils"
 	"github.com/JuanSamuelArbelaez/GO_API/controllers"
 	"github.com/gorilla/mux"
 	"log"
@@ -12,26 +11,26 @@ import (
 )
 
 func main() {
-	Utils.NotCountryCode("col")
-
 	router := mux.NewRouter()
 
 	SQL.InitDB()
-
+	router.HandleFunc("/misc/country/validate", controllers.ValidateCountryCode).Methods("GET")
 	router.HandleFunc("/people/add", controllers.AddPerson).Methods("PUT")
-
 	router.HandleFunc("/people/get-details", controllers.GetPersonDetails).Methods("GET")
 	router.HandleFunc("/people/get-all", controllers.GetAllPersonsDetails).Methods("GET")
 	router.HandleFunc("/people/count-all", controllers.CountPersons).Methods("GET")
+	router.HandleFunc("/people/remove", controllers.DeletePerson).Methods("DELETE")
+	router.HandleFunc("/people/recover", controllers.RecoverPerson).Methods("POST")
+	router.HandleFunc("/people/update", controllers.UpdatePerson).Methods("POST")
 
 	port := ":8087"
 
-	servidor := &http.Server{
+	server := &http.Server{
 		Handler:      router,
 		Addr:         "0.0.0.0" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 	fmt.Printf("Starting server on port %s. Press CTRL + C to escape\n", port)
-	log.Fatal(servidor.ListenAndServe())
+	log.Fatal(server.ListenAndServe())
 }
