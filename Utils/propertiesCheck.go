c94ef2aa27891cb8ea752391692aef7cff38e689
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-04-02 18:54:18 -0500

    Country code checker moved to country service.

--- Utils/propertiesCheck.go
+++ Utils/propertiesCheck.go
@@ -1,10 +1,6 @@
 package Utils
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/JuanSamuelArbelaez/GO_API/model"
-	"net/http"
 	"regexp"
 )
 
@@ -15,21 +11,4 @@ func EmptyString(str string) (isEmpty bool) {
 func NotEmailString(email string) (isNotEmail bool) {
 	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 	return !emailRegex.MatchString(email)
-}
-
-func NotCountryCode(countryCode string) (isNotCountry bool) {
-	url := fmt.Sprintf("https://restcountries.com/v3.1/alpha/%s", countryCode)
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("err:", err)
-		return true
-	}
-	decoder := json.NewDecoder(resp.Body)
-
-	var countries model.Countries
-	err = decoder.Decode(&countries)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return false
 }
